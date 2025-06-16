541c2d5223b81de020b41a880b2a50fddcda9999
Magicolo <magicololand@gmail.com>
2025-06-16 20:33:15 +0000

    Add new instrument implementations for ploop, prout, and super; define sound generation and initialization logic

---
+++ instruments/ploop.py
@@ -0,0 +1,22 @@
+from pyo import PyoObject, Sine, Change, TrigEnv, LinTable, SigTo
+
+
+def new(frequency: PyoObject, amplitude: PyoObject) -> PyoObject:
+    return Sine(
+        freq=frequency,
+        mul=TrigEnv(
+            Change(frequency),
+            table=LinTable([(0, 0), (100, 1), (500, 0.2), (8191, 0)]),
+            dur=0.2,
+            mul=amplitude * 4.0,
+        ),
+    )
+
+
+if __name__ == "__main__":
+    from pyo import Server
+
+    server = Server().boot().start()
+    instrument = new(SigTo(400), SigTo(0.1))
+    instrument.out()
+    server.gui(locals())
